1c329a302ae88d51601f05d023be8f4ca2eaa1e0
Appo <322@322.322>
2026-06-24 08:08:41 +0300

    Convert temperatures between Fahrenheit and Celsius.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 fn main() {
-    let c:char = 'a';
+   //Convert temperatures between Fahrenheit and Celsius.
+    // tC =  5/9 * (tF - 32)
+    // tF = 9/5 * tC + 32
+    println!("fahrenheit_to_celsius (98): {}\n
+    celsius_to_fahrenheit (18.2):{}",
+    (fahrenheit_to_celsius(98.0) * 10.0).round() / 10.0,
+    (celsius_to_fahrenheit(18.2) * 10.0).round() / 10.0);
 
-    println!("{}",size_of::<char>());
+}
+// tC =  5/9 * (tF - 32)
+fn fahrenheit_to_celsius(fahrenheit:f64)->f64{
+    5.0/9.0 * (fahrenheit - 32.0)
+
+}
+// tF = 9/5 * tC + 32
+fn celsius_to_fahrenheit(celsius:f64)->f64{
+    9.0/5.0 * celsius + 32.0
 }
