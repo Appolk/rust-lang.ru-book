5a8af5e420545aec0a65a1bf3eec2a8dc5c3a984
Appo <322@322.322>
2026-06-24 08:30:13 +0300

    Generate the nth Fibonacci number.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
+use std::io;
 fn main() {
-   //Convert temperatures between Fahrenheit and Celsius.
-    // tC =  5/9 * (tF - 32)
-    // tF = 9/5 * tC + 32
-    println!("fahrenheit_to_celsius (98): {}\n
-    celsius_to_fahrenheit (18.2):{}",
-    (fahrenheit_to_celsius(98.0) * 10.0).round() / 10.0,
-    (celsius_to_fahrenheit(18.2) * 10.0).round() / 10.0);
-
+   //Generate the nth Fibonacci number.
+    println!("input n:");
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("exception");
+    println!("{}",fibonacci(buffer.trim().parse().expect("not integer")));
 }
-// tC =  5/9 * (tF - 32)
-fn fahrenheit_to_celsius(fahrenheit:f64)->f64{
-    5.0/9.0 * (fahrenheit - 32.0)
+fn fibonacci(n:usize)->usize{
 
-}
-// tF = 9/5 * tC + 32
-fn celsius_to_fahrenheit(celsius:f64)->f64{
-    9.0/5.0 * celsius + 32.0
+    if n <=1{
+       return n;
+    }
+    let mut prev:usize = 0;
+    let mut curr:usize = 1;
+
+    for i in 2..=n {
+    let next = prev + curr;
+    println!("{}. {} + {} -> {}",i-2,prev,curr,next);
+    prev = curr;
+    curr = next;
+    }
+    curr
 }
